38360a0400b2335506011e824d713eaf55bef341
Sanjana-Redishetty <sanjana.redishetty@gmail.com>
2024-11-15 16:44:46 +0530

    Create Factorial 1.py

---
+++ visaa_prep/Factorial 1.py
@@ -0,0 +1,11 @@
+def fact(n):
+  if n==0:
+    return 1
+  result=1
+  for i in range(1,n+1):
+    result n*fact(n-1)
+  n = int(input())
+  if 1<=n<=15:
+    print(fact(n))
+  else:
+    print("1")
