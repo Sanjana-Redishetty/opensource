c1ae4708762d802fb5a68b9fcab255bed4207752
Sanjana-Redishetty <sanjana.redishetty@gmail.com>
2024-11-19 21:31:15 +0530

    Create rps.py

---
+++ visaa_prep/rps.py
@@ -0,0 +1,8 @@
+n=input().strip()
+Vignesh,Charan=n.split()
+if Vignesh==Charan:
+    print("NULL")
+elif(Vignesh=='R' and Charan == 'S') or (Vignesh == 'P' and Charan == 'R') or (Vignesh == 'S' and Charan=='P'):
+    print("Vignesh")
+else:
+    print("Charan")
